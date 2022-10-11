dc27909916472d58d24f1f2ae91249105c371569
Sanket Kapse <sanket8397@gmail.com>
2022-10-10 22:58:30 -0700

    Created code skeleton for Facade design pattern.

---
+++ src/ProductMenu.java
@@ -0,0 +1,8 @@
+public interface ProductMenu {
+    public void showMenu();
+    public void showAddButton();
+    public void showViewButton();
+    public void showRadioButton();
+    public void showLabels();
+    public void showComboxes();
+}
